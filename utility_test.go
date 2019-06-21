24e03a4560950ed4b2b4d8b20969da284b3fc07b
Fabio Alessandro Locati <me@fale.io>
2019-06-21 18:23:42 +0200

    more time safety functions

--- utility_test.go
+++ utility_test.go
@@ -5,13 +5,12 @@ import (
 	"time"
 )
 
-//func IsTimeSlotSafe(timeslot string) (bool, error) {
-func TestIsTimeSlotSafe(T *testing.T) {
+type DataPoint struct {
+	Time time.Time
+	Safe bool
+}
 
-	type DataPoint struct {
-		Time time.Time
-		Safe bool
-	}
+func TestIsTimeSlotSafe(T *testing.T) {
 
 	dps := []DataPoint{
 		{
@@ -26,6 +25,30 @@ func TestIsTimeSlotSafe(T *testing.T) {
 
 	for _, dp := range dps {
 		act, err := IsTimeSlotSafe(dp.Time.Format("2006-01-02-15"))
+		if err != nil {
+			T.Fatal(err)
+		}
+		if act != dp.Safe {
+			T.Errorf("%v was expected to be %v to safety while it was %v", dp.Time, dp.Safe, act)
+		}
+	}
+}
+
+func TestIsTimeSlotDaySafe(T *testing.T) {
+
+	dps := []DataPoint{
+		{
+			Time: time.Now().In(time.UTC).Add(-48 * time.Hour),
+			Safe: true,
+		},
+		{
+			Time: time.Now().In(time.UTC).Add(-24 * time.Hour),
+			Safe: false,
+		},
+	}
+
+	for _, dp := range dps {
+		act, err := IsTimeSlotDaySafe(dp.Time.Format("2006-01-02-15"))
 		if err != nil {
 			T.Fatal(err)
 		}
