2f1c73c49907a5b3812087a68d8b73b0d92d5b42
Fabio Alessandro Locati <me@fale.io>
2019-07-01 19:23:35 +0200

    improve test case to include CRLF

--- writer_test.go
+++ writer_test.go
@@ -10,6 +10,7 @@ import (
 type WriterTestCase struct {
 	Fields         []string
 	Entries        []map[string]string
+	UseCRLF        bool
 	ExpectedError  error
 	ExpectedOutput string
 }
@@ -27,12 +28,26 @@ var writerTestCases = []WriterTestCase{
 		ExpectedError:  nil,
 		ExpectedOutput: "#Version: 1.0\n#Fields: A\tB\tC\na\tb\tc\n",
 	},
+	WriterTestCase{
+		Fields:  []string{"A", "B", "C"},
+		UseCRLF: true,
+		Entries: []map[string]string{
+			map[string]string{
+				"C": "c",
+				"A": "a",
+				"B": "b",
+			},
+		},
+		ExpectedError:  nil,
+		ExpectedOutput: "#Version: 1.0\r\n#Fields: A\tB\tC\r\na\tb\tc\r\n",
+	},
 }
 
 func TestWriteAll(t *testing.T) {
 	for _, tc := range writerTestCases {
 		b := &bytes.Buffer{}
 		w := elf.NewWriter(b, tc.Fields)
+		w.UseCRLF = tc.UseCRLF
 		err := w.WriteAll(tc.Entries)
 		if err != tc.ExpectedError {
 			t.Errorf("Unexpected error: got %v wanted %v", err, tc.ExpectedError)
