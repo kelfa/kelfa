ac3d38c6f041cdb2eb3eaaa086afd9b65b2e3f34
Fabio Alessandro Locati <me@fale.io>
2019-07-01 19:16:31 +0200

    add license headers

--- reader.go
+++ reader.go
@@ -1,3 +1,7 @@
+// Copyright 2019 The Kelfe authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE.txt file.
+
 package elf
 
 import (
