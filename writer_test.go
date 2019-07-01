6378b72da0eb3159e8136af4084e9c5b6b14fb8c
Fabio Alessandro Locati <me@fale.io>
2019-07-01 19:15:23 +0200

    improve tests

--- writer_test.go
+++ writer_test.go
@@ -2,26 +2,44 @@ package elf_test
 
 import (
 	"bytes"
-	"fmt"
 	"testing"
 
 	"github.com/kelfa/elf"
 )
 
-func TestWriteAll(t *testing.T) {
-	b := &bytes.Buffer{}
-	w := elf.NewWriter(b, []string{"A", "B", "C"})
-	err := w.WriteAll(
-		[]map[string]string{
+type WriterTestCase struct {
+	Fields         []string
+	Entries        []map[string]string
+	ExpectedError  error
+	ExpectedOutput string
+}
+
+var writerTestCases = []WriterTestCase{
+	WriterTestCase{
+		Fields: []string{"A", "B", "C"},
+		Entries: []map[string]string{
 			map[string]string{
 				"C": "c",
 				"A": "a",
 				"B": "b",
 			},
-		})
-	if err != nil {
-		t.Errorf("Unexpected error:\ngot  %v\nwant %v", err, nil)
+		},
+		ExpectedError:  nil,
+		ExpectedOutput: "#Version: 1.0\n#Fields: A\tB\tC\na\tb\tc\n",
+	},
+}
+
+func TestWriteAll(t *testing.T) {
+	for _, tc := range writerTestCases {
+		b := &bytes.Buffer{}
+		w := elf.NewWriter(b, tc.Fields)
+		err := w.WriteAll(tc.Entries)
+		if err != tc.ExpectedError {
+			t.Errorf("Unexpected error: got %v wanted %v", err, tc.ExpectedError)
+		}
+		if b.String() != tc.ExpectedOutput {
+			t.Errorf("Unexpected output: got:\n%v\nwanted:\n%v", b.String(), tc.ExpectedOutput)
+			t.Errorf("Unexpected output: got:\n%v\nwanted:\n%v", b.Bytes(), []byte(tc.ExpectedOutput))
+		}
 	}
-	out := b.String()
-	fmt.Println(out)
 }
