0ce6ce6585d43534fcbecac91cdc18862359b171
Fabio Alessandro Locati <me@fale.io>
2019-07-03 22:44:03 +0200

    start working on usability aka documentation

--- reader.go
+++ reader.go
@@ -12,33 +12,71 @@ import (
 	"strings"
 )
 
+var (
+	ErrFieldCount = errors.New("wrong number of fields")
+)
+
+// A Reader reads records from an Extended Log Format file.
+//
+// As returned by NewReader, a Reader expects input conforming to the W3C ELF.
+// The exported fields are populated by the Read and the ReadAll functions.
+//
+// The Reader manages both \r\n and \n sequences.
 type Reader struct {
-	Fields    []string
-	Version   string
-	Software  string
+	// List of fields as for the Field descriptor.
+	// This field gets populated by reader during the read.
+	Fields []string
+	// Version of the ELF format used, as for the Version descriptor.
+	// This field gets populated by reader during the read.
+	Version string
+	// Software that created the ELF file, as for the Software descriptor.
+	// This field gets populated by reader during the read.
+	Software string
+	// StartDate of the oldest entry, as for the StartDate descriptor.
+	// This field gets populated by reader during the read.
 	StartDate string
-	EndDate   string
-	Date      string
-	Remark    string
+	// EndDate of the oldest entry, as for the EndDate descriptor.
+	// This field gets populated by reader during the read.
+	EndDate string
+	// Date of the oldest entry, as for the Date descriptor.
+	// This field gets populated by reader during the read.
+	Date string
+	// Remarks, as for the Remarks descriptor.
+	// This field gets populated by reader during the read.
+	Remark string
 
 	r         *bufio.Reader
 	rawBuffer []byte
 	numLine   int
 }
 
+// NewReader returns a new Reader that reads from r.
 func NewReader(r io.Reader) *Reader {
 	return &Reader{
 		r: bufio.NewReader(r),
 	}
 }
 
-func (r *Reader) Read() (map[string]string, error) {
-	record, err := r.readRecord()
+// Read reads one record (a slice of fields) from r.
+// If the record has an unexpected number of fields,
+// Read returns the error ErrFieldCount.
+//
+// Read always returns either a non-nil record or a non-nil error, but not both.
+// The only exception is if the line is a comment or empty, then both the
+// record and the error will be nil.
+//
+// If there is no data left to be read, Read returns nil, io.EOF.
+func (r *Reader) Read() (record map[string]string, err error) {
+	record, err = r.readRecord()
 	return record, err
 }
 
-func (r *Reader) ReadAll() ([]map[string]string, error) {
-	var records []map[string]string
+// ReadAll reads all the remaining records from r.
+// Each record is a map of fields.
+// A successful call returns err == nil, not err == io.EOF. Because ReadAll is
+// defined to read until EOF, it does not treat end of file as an error to be
+// reported.
+func (r *Reader) ReadAll() (records []map[string]string, err error) {
 	for {
 		record, err := r.readRecord()
 		if err == io.EOF {
@@ -74,7 +112,7 @@ func (r *Reader) readRecord() (map[string]string, error) {
 	}
 	fs := strings.Fields(string(bs))
 	if len(fs) != len(r.Fields) {
-		return nil, fmt.Errorf("was expecting %v columns while line %v has %v", len(r.Fields), r.numLine, len(fs))
+		return nil, ErrFieldCount
 	}
 	record := make(map[string]string)
 	for i, f := range fs {
