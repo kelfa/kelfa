449cb0195a670cf5b7ff29d74261d774caf233ed
Fabio Alessandro Locati <me@fale.io>
2019-07-14 08:16:22 +0200

    builder

---
+++ pkg/analytics/main.go
@@ -0,0 +1,43 @@
+package analytics
+
+import (
+	"fmt"
+	"time"
+
+	"go.kelfa.io/kelfa/pkg/dal"
+	"go.kelfa.io/kelfa/pkg/dal/objects"
+)
+
+type Analytics struct {
+	ds         *dal.DataSource
+	from       time.Time
+	to         time.Time
+	dataPoints []objects.DataPoint
+}
+
+func New(ds *dal.DataSource, from time.Time, to time.Time) (*Analytics, error) {
+	dsbt, err := (*ds).DataBeginTime()
+	if err != nil {
+		return nil, err
+	}
+	if from.Before(*dsbt) {
+		return nil, fmt.Errorf("the data start at %v so %v is an invalid starting point", dsbt, from)
+	}
+	dset, err := (*ds).DataEndTime()
+	if err != nil {
+		return nil, err
+	}
+	if to.After(*dset) {
+		return nil, fmt.Errorf("the data end at %v so %v is an invalid ending point", dset, to)
+	}
+	dsdps, err := (*ds).GetDataPoints(from, to)
+	if err != nil {
+		return nil, err
+	}
+	a := Analytics{
+		from:       from,
+		to:         to,
+		dataPoints: dsdps,
+	}
+	return &a, nil
+}
