02e34442bc17c7d0c2ff2547fd0a20633f6ef2eb
Fabio Alessandro Locati <me@fale.io>
2019-07-17 23:09:09 +0200

    imrpove code readability

--- pkg/analytics/cache_test.go
+++ pkg/analytics/cache_test.go
@@ -9,28 +9,26 @@ import (
 	"go.kelfa.io/kelfa/pkg/dal/objects"
 )
 
-type CacheTest struct {
-	From   time.Time
-	To     time.Time
-	Hits   int
-	Misses int
-}
-
-var cacheTestData = []CacheTest{
-	CacheTest{
-		From:   time.Date(2019, time.July, 01, 0, 0, 0, 0, time.UTC),
-		To:     time.Date(2019, time.July, 01, 23, 59, 59, 0, time.UTC),
-		Hits:   423,
-		Misses: 253,
-	},
-}
-
 func TestCacheStats(t *testing.T) {
+	tests := []struct {
+		From   time.Time
+		To     time.Time
+		Hits   int
+		Misses int
+	}{
+		{
+			From:   time.Date(2019, time.July, 01, 0, 0, 0, 0, time.UTC),
+			To:     time.Date(2019, time.July, 01, 23, 59, 59, 0, time.UTC),
+			Hits:   423,
+			Misses: 253,
+		},
+	}
+
 	data, err := dal.New("filesystem", objects.BackendOptions{Path: "../../data"})
 	if err != nil {
 		t.Errorf("an error occurred while creating the filesystem object: %v", err)
 	}
-	for _, dp := range cacheTestData {
+	for _, dp := range tests {
 		a, err := analytics.New(&data, dp.From, dp.To)
 		if err != nil {
 			t.Errorf("an error occurred while creating the analytics object: %v", err)
