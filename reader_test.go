1be755fae590799d0d9b2de5af95feec9ae0a5ef
Fabio Alessandro Locati <me@fale.io>
2019-07-17 22:51:56 +0200

    improve readability

--- reader_test.go
+++ reader_test.go
@@ -8,48 +8,52 @@ import (
 	"go.kelfa.io/elf"
 )
 
-type ReaderTestCase struct {
-	ELF             string
-	UseCRLF         bool
-	ExpectedError   error
-	ExpectedVersion string
-	ExpectedOutput  []map[string]string
-	ExpectedFields  []string
-}
-
-var readerTestCases = []ReaderTestCase{
-	{
-		ELF:             "#Version: 1.0\n#Date: 12-Jan-1996 00:00:00\n#Fields: time cs-method cs-uri\n00:34:23 GET /foo/bar.html\n12:21:16 GET /foo/bar.html\n12:45:52 GET /foo/bar.html\n12:57:34 GET /foo/bar.html",
-		ExpectedError:   nil,
-		ExpectedVersion: "1.0",
-		ExpectedFields:  []string{"time", "cs-method", "cs-uri"},
-		ExpectedOutput: []map[string]string{
-			{
-				"time":      "00:34:23",
-				"cs-method": "GET",
-				"cs-uri":    "/foo/bar.html",
-			},
-			{
-				"time":      "12:21:16",
-				"cs-method": "GET",
-				"cs-uri":    "/foo/bar.html",
-			},
-			{
-				"time":      "12:45:52",
-				"cs-method": "GET",
-				"cs-uri":    "/foo/bar.html",
-			},
-			{
-				"time":      "12:57:34",
-				"cs-method": "GET",
-				"cs-uri":    "/foo/bar.html",
+func TestReadAll(t *testing.T) {
+	tests := []struct {
+		ELF             string
+		UseCRLF         bool
+		ExpectedError   error
+		ExpectedVersion string
+		ExpectedOutput  []map[string]string
+		ExpectedFields  []string
+	}{
+		{
+			ELF: "#Version: 1.0\n" +
+				"#Date: 12-Jan-1996 00:00:00\n" +
+				"#Fields: time cs-method cs-uri\n" +
+				"00:34:23 GET /foo/bar.html\n" +
+				"12:21:16 GET /foo/bar.html\n" +
+				"12:45:52 GET /foo/bar.html\n" +
+				"12:57:34 GET /foo/bar.html",
+			ExpectedError:   nil,
+			ExpectedVersion: "1.0",
+			ExpectedFields:  []string{"time", "cs-method", "cs-uri"},
+			ExpectedOutput: []map[string]string{
+				{
+					"time":      "00:34:23",
+					"cs-method": "GET",
+					"cs-uri":    "/foo/bar.html",
+				},
+				{
+					"time":      "12:21:16",
+					"cs-method": "GET",
+					"cs-uri":    "/foo/bar.html",
+				},
+				{
+					"time":      "12:45:52",
+					"cs-method": "GET",
+					"cs-uri":    "/foo/bar.html",
+				},
+				{
+					"time":      "12:57:34",
+					"cs-method": "GET",
+					"cs-uri":    "/foo/bar.html",
+				},
 			},
 		},
-	},
-}
+	}
 
-func TestReadAll(t *testing.T) {
-	for _, tc := range readerTestCases {
+	for _, tc := range tests {
 		r := elf.NewReader(strings.NewReader(tc.ELF))
 		d, err := r.ReadAll()
 		if err != tc.ExpectedError {
--- writer_test.go
+++ writer_test.go
@@ -7,58 +7,56 @@ import (
 	"go.kelfa.io/elf"
 )
 
-type WriterTestCase struct {
-	Fields         []string
-	Entries        []map[string]string
-	UseCRLF        bool
-	ExpectedError  error
-	ExpectedOutput string
-}
-
-var writerTestCases = []WriterTestCase{
-	// Parameters inverted
-	{
-		Fields: []string{"A", "B", "C"},
-		Entries: []map[string]string{
-			{
-				"C": "c",
-				"A": "a",
-				"B": "b",
-			},
-		},
-		ExpectedError:  nil,
-		ExpectedOutput: "#Version: 1.0\n#Fields: A\tB\tC\na\tb\tc\n",
-	},
-	// Missing parameter
-	{
-		Fields: []string{"A", "B", "C"},
-		Entries: []map[string]string{
-			{
-				"A": "a",
-				"B": "b",
-			},
-		},
-		ExpectedError:  nil,
-		ExpectedOutput: "#Version: 1.0\n#Fields: A\tB\tC\na\tb\t-\n",
-	},
-	// CRLF
-	{
-		Fields:  []string{"A", "B", "C"},
-		UseCRLF: true,
-		Entries: []map[string]string{
-			{
-				"A": "a",
-				"B": "b",
-				"C": "c",
-			},
-		},
-		ExpectedError:  nil,
-		ExpectedOutput: "#Version: 1.0\r\n#Fields: A\tB\tC\r\na\tb\tc\r\n",
-	},
-}
-
 func TestWriteAll(t *testing.T) {
-	for _, tc := range writerTestCases {
+	tests := []struct {
+		Fields         []string
+		Entries        []map[string]string
+		UseCRLF        bool
+		ExpectedError  error
+		ExpectedOutput string
+	}{
+		// Parameters inverted
+		{
+			Fields: []string{"A", "B", "C"},
+			Entries: []map[string]string{
+				{
+					"C": "c",
+					"A": "a",
+					"B": "b",
+				},
+			},
+			ExpectedError:  nil,
+			ExpectedOutput: "#Version: 1.0\n#Fields: A\tB\tC\na\tb\tc\n",
+		},
+		// Missing parameter
+		{
+			Fields: []string{"A", "B", "C"},
+			Entries: []map[string]string{
+				{
+					"A": "a",
+					"B": "b",
+				},
+			},
+			ExpectedError:  nil,
+			ExpectedOutput: "#Version: 1.0\n#Fields: A\tB\tC\na\tb\t-\n",
+		},
+		// CRLF
+		{
+			Fields:  []string{"A", "B", "C"},
+			UseCRLF: true,
+			Entries: []map[string]string{
+				{
+					"A": "a",
+					"B": "b",
+					"C": "c",
+				},
+			},
+			ExpectedError:  nil,
+			ExpectedOutput: "#Version: 1.0\r\n#Fields: A\tB\tC\r\na\tb\tc\r\n",
+		},
+	}
+
+	for _, tc := range tests {
 		b := &bytes.Buffer{}
 		w := elf.NewWriter(b, tc.Fields)
 		w.UseCRLF = tc.UseCRLF
