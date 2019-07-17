a7d6e33016eb1361556a25c3dcd351cf46ba472f
Fabio Alessandro Locati <me@fale.io>
2019-07-17 23:09:56 +0200

    fix pointer to reused variable

--- pkg/analytics/session.go
+++ pkg/analytics/session.go
@@ -5,6 +5,7 @@ import "go.kelfa.io/kelfa/pkg/session"
 func (a *Analytics) GetSessions() *session.Sessions {
 	var ss session.Sessions
 	for _, s := range a.dataPoints {
+		s := s
 		ss.AddDataPoint(&s)
 	}
 	return &ss
