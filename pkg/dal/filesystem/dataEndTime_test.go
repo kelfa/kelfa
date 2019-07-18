cc11cbddf0d631fba9aa53154430832b60fbc38a
Fabio Alessandro Locati <me@fale.io>
2019-07-18 18:34:21 +0200

    improve tests

---
+++ pkg/dal/filesystem/dataEndTime_test.go
@@ -0,0 +1,34 @@
+package filesystem_test
+
+import (
+	"testing"
+	"time"
+
+	"go.kelfa.io/kelfa/pkg/dal/filesystem"
+	"go.kelfa.io/kelfa/pkg/dal/objects"
+)
+
+func TestDataEndTime(t *testing.T) {
+	tests := []struct {
+		folder       string
+		expectedTime time.Time
+	}{
+		{
+			folder:       "../../../test/data/logs",
+			expectedTime: time.Date(2019, time.July, 1, 23, 59, 59, 0, time.UTC),
+		},
+	}
+	for _, test := range tests {
+		f, err := filesystem.New(objects.BackendOptions{Path: test.folder})
+		if err != nil {
+			t.Errorf("an error occurred while creating the filesystem object: %v", err)
+		}
+		bt, err := f.DataEndTime()
+		if err != nil {
+			t.Errorf("an error occurred while looking for the begin time: %v", err)
+		}
+		if *bt != test.expectedTime {
+			t.Errorf("expecting %v, received %v", test.expectedTime, bt)
+		}
+	}
+}
