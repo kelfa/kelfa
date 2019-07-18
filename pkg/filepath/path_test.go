cbd3dec61d3c11678ca0936691f1a1b1cdd4fdae
Fabio Alessandro Locati <me@fale.io>
2019-07-18 18:47:23 +0200

    simplify example

--- pkg/filepath/path_test.go
+++ pkg/filepath/path_test.go
@@ -6,20 +6,18 @@ import (
 	"go.kelfa.io/kelfa/pkg/filepath"
 )
 
-type PathTC struct {
-	Path           string
-	ExpectedOutput string
-}
-
-var PathTestCases = []PathTC{
-	{Path: "/home/test/file.txt", ExpectedOutput: "/home/test"}, // Standard file
-	{Path: "/home/test/file", ExpectedOutput: "/home/test"},     // File without extension
-	{Path: "/home/test/", ExpectedOutput: "/home/test"},         // Folder with tailing slash
-	{Path: "/home/test", ExpectedOutput: "/home"},               // Folder with no tailing slash
-}
-
 func TestPath(t *testing.T) {
-	for _, tc := range PathTestCases {
+	tests := []struct {
+		Path           string
+		ExpectedOutput string
+	}{
+		{Path: "/home/test/file.txt", ExpectedOutput: "/home/test"}, // Standard file
+		{Path: "/home/test/file", ExpectedOutput: "/home/test"},     // File without extension
+		{Path: "/home/test/", ExpectedOutput: "/home/test"},         // Folder with tailing slash
+		{Path: "/home/test", ExpectedOutput: "/home"},               // Folder with no tailing slash
+	}
+
+	for _, tc := range tests {
 		if filepath.Path(tc.Path) != tc.ExpectedOutput {
 			t.Fatalf("expecting %s, got %s", tc.ExpectedOutput, filepath.Path(tc.Path))
 		}
