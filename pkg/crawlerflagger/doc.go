1178b497b46fc8042114f4167ae66ed4c9c80afa
Fabio Alessandro Locati <me@fale.io>
2019-07-23 21:54:41 +0200

    add benchmarks

---
+++ pkg/crawlerflagger/doc.go
@@ -0,0 +1,13 @@
+//  BenchmarkExactMatch/case0-8             10000000               182 ns/op
+//  BenchmarkExactMatch/case101-8           10000000               128 ns/op
+//  BenchmarkExactMatch/case200-8           10000000               137 ns/op
+//  BenchmarkExactMatch/case300-8           10000000               124 ns/op
+//  BenchmarkExactMatch/case400-8           20000000               113 ns/op
+//  BenchmarkExactMatch/miss-8              200000000                8.03 ns/op
+//  BenchmarkRegExpMatch/case0-8             5000000               292 ns/op
+//  BenchmarkRegExpMatch/case101-8            200000              7335 ns/op
+//  BenchmarkRegExpMatch/case200-8            100000             12866 ns/op
+//  BenchmarkRegExpMatch/case300-8            100000             21898 ns/op
+//  BenchmarkRegExpMatch/case400-8             50000             26515 ns/op
+//  BenchmarkRegExpMatch/miss-8                50000             23963 ns/op
+package crawlerflagger
