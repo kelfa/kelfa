ed841549ee97063f333f4a2e62369d3a4493b193
Fabio Alessandro Locati <me@fale.io>
2019-07-28 16:25:29 +0200

    mode parsing for easier usage

--- pkg/analytics/main.go
+++ pkg/analytics/main.go
@@ -19,6 +19,23 @@ const (
 	Monthly
 )
 
+func ParseMode(s string) (Mode, error) {
+	switch s {
+	case "none":
+		return None, nil
+	case "hourly":
+		return Hourly, nil
+	case "daily":
+		return Daily, nil
+	case "weekly":
+		return Weekly, nil
+	case "monthly":
+		return Monthly, nil
+	default:
+		return None, fmt.Errorf("the '%v' mode is not valid. Valid modes are: none, hourly, daily, weekly, monthly")
+	}
+}
+
 type DataSet struct {
 	Begin    time.Time
 	End      time.Time
