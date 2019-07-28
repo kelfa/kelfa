d9bd9833db96b8bd52c05e67dd4db704e72ff9b0
Fabio Alessandro Locati <me@fale.io>
2019-07-28 16:08:21 +0200

    shift cache metrics logic to sessions instead of analytics

--- pkg/analytics/cache.go
+++ pkg/analytics/cache.go
@@ -1,36 +1,12 @@
 package analytics
 
-import "go.kelfa.io/kelfa/pkg/dal/objects"
+import "go.kelfa.io/kelfa/pkg/session"
 
-func (a *Analytics) CacheDataAvailable() bool {
-	return len(a.Data[0].Sessions.Sessions[0].DataPoints[0].CDNCache) == 0
-}
-
-func (a *Analytics) CacheStats() *objects.CacheStats {
-	cs := objects.CacheStats{}
+func (a *Analytics) CacheStats() *session.CacheStats {
+	cs := session.CacheStats{}
 	for _, ds := range a.Data {
-		for _, s := range ds.Sessions.Sessions {
-			for _, log := range s.DataPoints {
-				cs.Total++
-				switch log.CDNCache {
-				case "Hit":
-					cs.Hits++
-				case "RefreshHit":
-					cs.RefreshHits++
-				case "Miss":
-					cs.Misses++
-				case "LimitExceeded":
-					cs.LimitExceeded++
-				case "CapacityExceeded":
-					cs.CapacityExceeded++
-				case "Error":
-					cs.Errors++
-				case "Redirect":
-					cs.Redirects++
-				default:
-					cs.Others++
-				}
-			}
+		for _, s := range ds.Sessions {
+			cs.Add(s.CacheStats())
 		}
 	}
 	return &cs
