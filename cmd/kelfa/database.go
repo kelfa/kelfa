8b5d30045d4947f2b6fcb45eeca76a617fd69cc4
Fabio Alessandro Locati <me@fale.io>
2019-12-27 07:59:10 +0100

    merge sqlliteImporter in kelfa core

--- cmd/kelfa/database.go
+++ cmd/kelfa/database.go
@@ -5,43 +5,52 @@ import (
 	"log"
 	"os"
 
+	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"go.kelfa.io/kelfa/pkg/dal"
 	"go.kelfa.io/kelfa/pkg/dal/objects"
 )
 
-func main() {
-	viper.SetConfigType("yaml")
-	viper.SetConfigName("config")
-	viper.AddConfigPath("$HOME/.kelfa")
-	err := viper.ReadInConfig()
-	if err != nil {
-		fmt.Printf("fatal error config file: %s \n", err)
-		return
-	}
+func init() {
+	rootCmd.AddCommand(dbCmd)
+	dbCmd.AddCommand(dbImportCmd)
+}
+
+var dbCmd = &cobra.Command{
+	Use:   "database",
+	Short: "see hits rates",
+}
+
+var dbImportCmd = &cobra.Command{
+	Use:   "import",
+	Short: "import data",
+	RunE:  dbImport,
+}
+
+func dbImport(cmd *cobra.Command, args []string) error {
 	fs, err := dal.New("filesystem", objects.BackendOptions{Path: viper.GetString("data_folder")})
 	if err != nil {
-		log.Fatalf("%v", err)
+		return err
 	}
 	sql, err := dal.NewWritableDataSource("sqlite", objects.BackendOptions{Path: fmt.Sprintf("%s/.kelfa/database.db", os.Getenv("HOME"))})
 	if err != nil {
-		log.Fatalf("%v", err)
+		return err
 	}
 	fromTime, err := fs.DataBeginTime()
 	if err != nil {
-		log.Fatalf("%v", err)
+		return err
 	}
 	toTime, err := fs.DataEndTime()
 	if err != nil {
-		log.Fatalf("%v", err)
+		return err
 	}
 	dps, err := fs.GetDataPoints(*fromTime, *toTime)
 	if err != nil {
-		log.Fatalf("%v", err)
+		return err
 	}
 	dpssql, err := sql.GetDataPoints(*fromTime, *toTime)
 	if err != nil {
-		log.Fatalf("%v", err)
+		return err
 	}
 	for _, dp := range dps {
 		var present bool
@@ -61,4 +70,5 @@ func main() {
 			fmt.Printf("Not going to add %s %s. Already present\n", dp.DateTime, dp.ID)
 		}
 	}
+	return nil
 }
